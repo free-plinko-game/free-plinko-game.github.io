63e6a07020a6c5e5acd8c47899d065f542d3fd75
Ashley Hughes <althughes@Ashleys-MacBook-Air.local>
2025-11-13 11:19:26 +0000

    Fix build: remove hardcoded CSS preload links

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,5 @@
 <script lang="ts">
   import '../app.css';
-  import { page } from '$app/stores';
 </script>
-
-<svelte:head>
-  <!-- Preload critical CSS to start downloading immediately -->
-  <link rel="preload" href="/_app/immutable/assets/0.6WsFQ-g0.css" as="style" />
-  <link rel="preload" href="/_app/immutable/assets/3.BgpoJHDx.css" as="style" />
-</svelte:head>
 
 <slot />
